9d6ba94fb87c57c9c2f0dd0bc08bcb047d413268
Julio Rios <julio.rios.araujo@gmail.com>
2026-04-07 21:06:02 +0100

    removing unused file

--- frontend/src/pages/QuizFlowA.tsx
+++ frontend/src/pages/QuizFlowA.tsx
@@ -1,6 +1,33 @@
+import { useEffect } from 'react';
 import { QuizFlow } from './QuizFlow';
 import { quizConfigA } from '../config/quizConfigA';
 
 export function QuizFlowA() {
+  useEffect(() => {
+    // UTM script
+    const utmScript = document.createElement('script');
+    utmScript.src = 'https://cdn.utmify.com.br/scripts/utms/latest.js';
+    utmScript.setAttribute('data-utmify-prevent-subids', '');
+    utmScript.async = true;
+    utmScript.defer = true;
+    document.head.appendChild(utmScript);
+
+    // Pixel 1
+    window.pixelId = '69d4f55fd0d2be68368a4ede';
+    const pixel1 = document.createElement('script');
+    pixel1.async = true;
+    pixel1.defer = true;
+    pixel1.src = 'https://cdn.utmify.com.br/scripts/pixel/pixel.js';
+    document.head.appendChild(pixel1);
+
+    // Pixel 2
+    window.pixelId = '69d530814790c231cc0aeb59';
+    const pixel2 = document.createElement('script');
+    pixel2.async = true;
+    pixel2.defer = true;
+    pixel2.src = 'https://cdn.utmify.com.br/scripts/pixel/pixel.js';
+    document.head.appendChild(pixel2);
+  }, []);
+
   return <QuizFlow config={quizConfigA} returnPath="/a" />;
 }
--- frontend/src/pages/QuizFlowB.tsx
+++ frontend/src/pages/QuizFlowB.tsx
@@ -1,6 +1,33 @@
+import { useEffect } from 'react';
 import { QuizFlow } from './QuizFlow';
 import { quizConfigB } from '../config/quizConfigB';
 
 export function QuizFlowB() {
+  useEffect(() => {
+    // UTM script
+    const utmScript = document.createElement('script');
+    utmScript.src = 'https://cdn.utmify.com.br/scripts/utms/latest.js';
+    utmScript.setAttribute('data-utmify-prevent-subids', '');
+    utmScript.async = true;
+    utmScript.defer = true;
+    document.head.appendChild(utmScript);
+
+    // Pixel 1
+    window.pixelId = '69d530303c3b6ba88b6e7178';
+    const pixel1 = document.createElement('script');
+    pixel1.async = true;
+    pixel1.defer = true;
+    pixel1.src = 'https://cdn.utmify.com.br/scripts/pixel/pixel.js';
+    document.head.appendChild(pixel1);
+
+    // Pixel 2
+    window.pixelId = '69d530a5aa3bcf4239e0594a';
+    const pixel2 = document.createElement('script');
+    pixel2.async = true;
+    pixel2.defer = true;
+    pixel2.src = 'https://cdn.utmify.com.br/scripts/pixel/pixel.js';
+    document.head.appendChild(pixel2);
+  }, []);
+
   return <QuizFlow config={quizConfigB} returnPath="/b" />;
 }
